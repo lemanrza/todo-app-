f963dea79a40dbd76f9a353da0ca467e5db7650e
lemanrza <lemaner@code.edu.az>
2025-01-26 04:33:34 +0400

    finished

---
+++ js/validation.js
@@ -0,0 +1,78 @@
+const addBtn = document.querySelector(".add-btn")
+const clearBtn = document.querySelector(".clear-btn")
+const toDo = document.querySelector("ul")
+const inputField = document.querySelector("input");
+const taskCount = document.querySelector("span")
+let tasksNum = 0
+
+function createItem(taskText) {
+    const item = document.createElement("li")
+    item.innerHTML = `<div class="task">
+    <p class="forDoing">${taskText}</p>
+    <div class="btns">
+    <button class="click-btn"><i class="fa-solid fa-check"></i></button>
+    <button class="delete-btn"><i class="fa-solid fa-trash"></i></button>
+    <button class="edit-btn"><i class="fa-regular fa-pen-to-square"></i></button>
+    </div>
+    </div>`
+    const text = item.querySelector(".forDoing")
+    const clickBtn = item.querySelector(".click-btn")
+    clickBtn.addEventListener("click", function () {
+        text.classList.toggle("completed")
+        if (text.classList.contains("completed")) {
+            updateTaskCount(-1)
+        } else {
+            updateTaskCount(1)
+        }
+    })
+
+    const deleteBtn = item.querySelector(".delete-btn")
+    deleteBtn.addEventListener("click", function () {
+        item.remove()
+        updateTaskCount(-1)
+    })
+    return item;
+
+}
+addBtn.addEventListener("click", function (e) {
+    e.preventDefault()
+    const taskText = inputField.value
+    if (taskText) {
+        const item = createItem(taskText)
+        toDo.append(item)
+        toDo.classList.replace("d-none", "d-flex");
+        inputField.value = "";
+        Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Your work has been saved",
+            showConfirmButton: false,
+            timer: 1500
+        });
+        updateTaskCount(1);
+    }
+    else {
+        Swal.fire("Input is empty!");
+    }
+})
+clearBtn.addEventListener("click", function () {
+    toDo.innerHTML = ''
+    toDo.classList.replace("d-flex", "d-none")
+    resetTaskCount()
+})
+function updateTaskCount(count) {
+    tasksNum += count;
+    if (tasksNum < 0) {
+        tasksNum = 0
+    }
+    else {
+        taskCount.textContent = tasksNum === 0 ? "no" : tasksNum;
+    }
+}
+function resetTaskCount() {
+    tasksNum = 0
+    taskCount.textContent = "no"
+}
+
+
+
